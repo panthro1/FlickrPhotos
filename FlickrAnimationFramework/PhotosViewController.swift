7f156d53c9205f70d0dd85e5427bce39818d871d
panthro1 <ledesmajohn85@gmail.com>
2018-07-16 23:12:25 -0400

    Initial Commit

---
+++ FlickrAnimationFramework/PhotosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FlickrPhotos
+//
+//  Created by john ledesma on 7/16/18.
+//  Copyright © 2018 john ledesma. All rights reserved.
+//
+
+import UIKit
+
+
+public class PhotosViewController: UIViewController  {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
